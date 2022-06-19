440fd0d556a4df89e5af30ee1497be09ac14c9c3
David <david_why@outlook.com>
2022-06-19 18:36:30 +0800

    feat: Remove unused defs

--- src/testmode.c
+++ src/testmode.c
@@ -5,12 +5,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#define textFlags (*(uint8_t *)(0xD00085))
-#define trigFlags (*(uint8_t *)(0xD00080))
-#define statusBarFlags (*(uint8_t *)(0xD000C7))
-
-#define invcol() (textFlags ^= 1 << 3)
-
 const uint16_t testmode_pwd[] = {kCapT, kCapE, kCapS, kCapT, k1};
 
 void do_toggle_testmodebar()

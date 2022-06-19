8c055502d722147e79cda72ec041ce7229054e29
David <david_why@outlook.com>
2022-06-19 18:25:00 +0800

    feat: Add version def

--- src/defs.h
+++ src/defs.h
@@ -1,6 +1,9 @@
 #ifndef _DEFS_H
 #define _DEFS_H
 
+#define aaconsle_version "v1.0"
+#define aaconsle_version_major 1
+#define aaconsle_version_minor 0
 #define aaconsle_appvar_name "\x01" \
                              "ACONSLE"
 
